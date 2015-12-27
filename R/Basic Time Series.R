5aa080494863589ffaa195a33e547f4900f8b716
Flavio Clesio <flavioclesio@yahoo.com>
2015-12-27 09:52:48 -0200

    Ajustments in series

---
+++ R/Basic Time Series.R
@@ -0,0 +1,11 @@
+
+
+oil_production <- read.csv('https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/oil_production_2000_2015.csv')
+
+# Check variables data types
+str(oil_production)
+
+#Basic statistics
+summary(oil_production)
+
+
