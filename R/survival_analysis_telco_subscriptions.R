2665ecf3c867db337f6b3ac0e72841d552b0ce2d
Flavio Clesio <flavioclesio@yahoo.com>
2015-12-27 08:15:55 -0200

    Ship it! Survival analysis using telco subscriptions

--- R/survival_analysis_telco_subscriptions.R
+++ R/survival_analysis_telco_subscriptions.R
@@ -1,31 +1,23 @@
-# Um dos maiores desafios em serviços de assinaturas é manter a 
-# recorrencia do fluxo de receita, isso é, um modelo de gestão [1], [2] 
-# de assunaturas passa obrigatóriamente pelo fato de que a retenção dos
-# clientes é o aspecto mais importante de toda a cadeia de receita.
-# 
-# No entanto quando essa cadeia é quebrada, isso significa que o cliente
-# saiu por N motivos, e assim fica caracterizado o churn. 
-# 
-# Neste caso vamos usar Survival Analysis [3] para realizar uma estimativa
-# do tempo em que o usuário fica na base até o evento de churn (falha/morte)
+# One of the biggest challenges is to keep the subscriptions services is the recurring of the revenue stream.
+# It means a management model of subscriptions where retention of the customers is the most important aspect 
+# of the entire revenue.
 
-# Na análise usaremos o estimador Kaplan-Meier [4] para estimar o tempo
-# de vida da assinatura, dado que teremos censura à direita. 
-# 
-# Em outras palavras vamos estimar dado um deteminado período sem saber
-# ao certo o plano de futuro que iremos ter até o que o cliente venha a
-# sair da base.
+# However when this chain is broken, it means that the client out of the service for no reason, 
+# and it's called Churn.
 
+# In this case let's use Survival Analysis to estimate the time when the user is at the base until
+# the event of churn (failure/death)
 
+# The first analysis will use the Kaplan-Meier estimator to estimate the time signature lifetime, 
+# given that we have censorship on the right.
 
+# The second one will use cox proportional hazards model to see the importance of each variable in churn. 
+
+# In other words vet's estimate given a determined period without knowing the future if the customer will leave
+# and the main aspects of our dataset that can help to explain this. 
 
 # Load Survival package
 library(survival)
-
-# Load splines library.
-library(splines)
-
-
 
 #Read CSV file
 telco_subs <- read.csv('https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/telco_subscriptions.csv')
@@ -36,7 +28,11 @@ head(telco_subs)
 #Basic statistics
 summary(telco_subs)
 
+##Check variables
+names(telco_subs)
 
+##Check variables data types
+str(telco_subs)
 
 
 
@@ -64,26 +60,13 @@ axis(2, at=pretty(fit$surv), lab=paste0(pretty(fit$surv) * 100, " %"), las=TRUE)
 
 
 
+
+
 #For now we'll check some variables that make the churn rise!
-#To do this will use Kaplan-Meier Estimator (https://en.wikipedia.org/wiki/Kaplan%E2%80%93Meier_estimator)
-#We'll use this approach 'cause lot of our data is right-censored, i.e.
-#if we don't have the event of churn (failure/death) we'll assume that the subscription is active
-# The formula is: \hat S(t) = \prod\limits_{t_i<t} \frac{n_i-d_i}{n_i}.
 
-#We'll use 3 variables to the first analysis: has_billed, user_plan, renew_count
-#We'll use the same dataframe and same survival function
 
-#Read CSV file
-telco_subs <- read.csv('https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/telco_subscriptions.csv')
 
-#Check header
-head(telco_subs)
 
-##Check variables
-names(telco_subs)
-
-##Check variables data types
-str(telco_subs)
 
 #Calls function
 telco_subs$survival <- Surv(telco_subs$survival_days, telco_subs$churned == 1)
@@ -125,10 +108,11 @@ title(main = "Survival Curves in Telcom Subscriptions by Billing")
 abline(v=(seq(0,180,7)), col="gray", lty="dotted")
 abline(h=(seq(0,1,.10)), col="gray", lty="dotted")
 
-
-
 #Log-Rank Test
 survdiff(survival ~ has_billed, data = telco_subs)
+
+
+
 
 
 #Fit the survival function using variable user_plan
@@ -167,29 +151,54 @@ title(main = "Survival Curves in Telcom Subscriptions by User Plan")
 abline(v=(seq(0,180,7)), col="gray", lty="dotted")
 abline(h=(seq(0,1,.10)), col="gray", lty="dotted")
 
-
 #Log-Rank Test
 survdiff(survival ~ user_plan, data = telco_subs)
 
 
 
-s <- with(telco_subs, Surv(telco_subs$survival_days, telco_subs$churned))
 
-##Fit a cox proportional hazards model
-model <- coxph(s ~ telco_subs$created_as_free_user + telco_subs$has_billed + telco_subs$user_plan + telco_subs$billing_count , data=telco_subs)
 
-summary(model)
+# Let's use the Cox Hazard Model  
+cox_subs <- with(telco_subs, Surv(telco_subs$survival_days, telco_subs$churned))
 
-##Plot final result
-plot(survfit(model), col=c("blue", "red"))
+# Fitting the model using the variables created_as_free_user, has_billed, user_plan, billing_count 
+fit_cox_subs <- coxph(cox_subs ~ telco_subs$created_as_free_user + telco_subs$has_billed + telco_subs$user_plan + telco_subs$billing_count, data=telco_subs)
 
+# Get info about the model 
+summary(fit_cox_subs)
+
+# Plotting the model 
+plot(survfit(fit_cox_subs)
+     , mark.time = FALSE
+     , xlim=c(1,180) # I censored in 180 days, 'cause this is where the survival curve convenges to zero
+     , ylim=c(.00,1)
+     , xaxt='n'
+     , yaxt='n'
+     , xlab = '# Days since Subscribing'
+     , ylab = '% Retention Subscribers'
+  )
+
+# Format axis
+axis(side=1, at=seq(0, 180, by=14))
+axis(side=2, at=seq(0, 1, by=.10), labels=paste(seq(0,100,10), "%", sep=""), las=TRUE)
+
+title(main = "Survival Curves in Telcom Subscriptions")
+
+abline(v=(seq(0,180,7)), col="black", lty="dotted")
+abline(h=(seq(0,1,.10)), col="black", lty="dotted")
+
+
+
+
+
+#############
 # References
-# http://daynebatten.com/2015/02/customer-churn-survival-analysis/
-# http://daynebatten.com/2015/02/customer-churn-cox-regression/
-# http://stackoverflow.com/questions/27080207/survival-analysis-for-telecom-churn-using-r
-## Reference [1]: http://stackoverflow.com/questions/27080207/survival-analysis-for-telecom-churn-using-r
-## Reference [2]: http://daynebatten.com/2015/02/customer-churn-survival-analysis/
-## Reference [3]: https://en.wikipedia.org/wiki/Survival_analysis
-## Reference [4]: https://en.wikipedia.org/wiki/Kaplan%E2%80%93Meier_estimator
+#############
 
-
+# [1]: http://stackoverflow.com/questions/27080207/survival-analysis-for-telecom-churn-using-r
+# [2]: http://daynebatten.com/2015/02/customer-churn-survival-analysis/
+# [3]: https://en.wikipedia.org/wiki/Survival_analysis
+# [4]: https://en.wikipedia.org/wiki/Kaplan%E2%80%93Meier_estimator
+# [5]: http://daynebatten.com/2015/02/customer-churn-survival-analysis/
+# [6]: http://daynebatten.com/2015/02/customer-churn-cox-regression/
+# [7]: http://stackoverflow.com/questions/27080207/survival-analysis-for-telecom-churn-using-r
