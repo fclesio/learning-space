3b26b3dd347ddd4ddd2bea870cd018cb0a871303
fclesio <flavioclesio@yahoo.com>
2016-03-26 11:42:43 -0300

    Redefine aphorisms, consolidate rap songs, and reduce botaphorism interval

--- Python/ManoBot.py
+++ Python/ManoBot.py
@@ -2,10 +2,10 @@
 import tweepy, time
  
 # Consumer keys and access tokens, used for OAuth
-consumer_key = 'OXMzXnCySStI19mMXOusX0e6O'
-consumer_secret = 'DLi82wPr9ytwtutYWo8u8haKxoJQUQs3ww6Oyj32RprdLPLe7n'
-access_token = '713500052329787397-XSMFh3YbXFOEfbrD8zsfmRrYUjPBolt'
-access_token_secret = '1zfymH1OtEHU6MpDKZnSBW4ElL9mSSZV83NRQ6AgRCYf9'
+consumer_key = 'OeVNChP86yaMOUcjmWR2qHNJX'
+consumer_secret = 'mOFNF5Sl4IaXbM3lIHUnoTzsx3f7bYrnZ538ik8pe19sg0hN4x'
+access_token = '713725794204454913-FQKf50gCkUlEGCeXULj2WHzbO9vixLR'
+access_token_secret = 'pmY46883fkuZIHgQjs1PdNLsScEFoYPHGzhPPwG6bj4MS'
  
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -15,15 +15,15 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # File the bot will tweet from
-filename=open('aforismos.txt','r')
+filename=open('rap.txt','r')
 
 f=filename.readlines()
 
 filename.close()
 
 
-#Tweet a line every second
+#Tweet a line every 1 minute
 for line in f:
      api.update_status(line)
      print (line)
-     time.sleep(1)
+     time.sleep(60)
