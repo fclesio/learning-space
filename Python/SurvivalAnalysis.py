65b25a4614f61cd79680a943fe6c366e0f5ac2e3
fclesio <flavioclesio@yahoo.com>
2016-03-27 07:53:37 -0300

    Final commit of tutorial

--- Python/SurvivalAnalysis.py
+++ Python/SurvivalAnalysis.py
@@ -56,3 +56,38 @@ regression_dataset = load_regression_dataset()
 regression_dataset.head()
 
 
+
+
+
+
+from lifelines import AalenAdditiveFitter, CoxPHFitter
+
+# Using Cox Proportional Hazards model
+cf = CoxPHFitter()
+cf.fit(regression_dataset, 'T', event_col='E')
+cf.print_summary()
+
+# Using Aalen's Additive model
+aaf = AalenAdditiveFitter(fit_intercept=False)
+aaf.fit(regression_dataset, 'T', event_col='E')
+
+
+
+
+
+
+x = regression_dataset[regression_dataset.columns - ['E','T']]
+aaf.predict_survival_function(x.ix[10:12]).plot() #get the unique survival functions of the first two subjects
+
+
+
+aaf.plot()
+
+
+
+
+
+
+
+
+
