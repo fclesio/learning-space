230ba056e8d94b5b9284947bf4fa2b8e861a0075
fclesio <flavioclesio@yahoo.com>
2016-05-15 12:11:23 -0300

    Clean-up and modifications in CSV

---
+++ R/MARS - Post.R
@@ -0,0 +1,15 @@
+#Install packages
+install.packages("caret")
+install.packages("earth")
+
+#Recall libraries
+library(caret)
+library(earth)
+
+
+# Get data
+nubank_train <- read.csv('/Users/flavio.clesio/Downloads/data-science-puzzle/train.csv');
+
+
+
+https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/imoveis-tambore-parnaiba-2013.csv
