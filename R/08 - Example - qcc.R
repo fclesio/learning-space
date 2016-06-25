a63395a27fef06121bdb11521b1df84bc1c8391a
Flavio Clesio <flavioclesio@yahoo.com>
2016-06-25 14:04:48 -0300

    Ajustments in folders

--- R/08 - Example - qcc.R
+++
@@ -1,8 +0,0 @@
-library(qcc)
-
-# series of value w/ mean of 10 with a little random noise added in
-x <- rep(10, 100) + rnorm(100)
-# a test series w/ a mean of 11
-new.x <- rep(11, 15) + rnorm(15)
-# qcc will flag the new points
-qcc(x, newdata=new.x, type="xbar.one")
--- R/Correlation Test.R
+++
@@ -1,6 +0,0 @@
-#Correlation Test
-
-A <- c(7,2,4,8,7,3,9,10)
-B <- c(2,5,4,0,1,4,1,0)
-
-cor.test(B,A)
