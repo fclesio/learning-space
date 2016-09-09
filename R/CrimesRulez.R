b6de01196b96cfa648de6da048d96639db130327
flavio-clesio-movile <flavio.clesio@movile.com>
2016-09-09 00:08:02 -0300

    Some rules discovered

--- R/CrimesRulez.R
+++ R/CrimesRulez.R
@@ -1,40 +1,53 @@
+
+# Load arules library
 library(arules)
 
-#Load Dataset
-crimes <- read.csv("/Users/flavio.clesio/Desktop/crimes.csv")
+# Load Dataset
+crimes <- read.csv("https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/01%20-%20Association%20Rules/Crimes.csv")
 
-#See variables
+# See variables types
 str(crimes)
 
-#See basic statistics
+# See basic stats
 summary(crimes)
 
-# Construct the model, using a medium support and a mid-high confidence. 
-# The parameters of lengh was keeped 'cause we want no obvious rules and more actionable.
-model <- apriori(crimes
-                 ,parameter = list(minlen=4,maxlen=10, supp=0.55, conf=0.80))
+# The most important thing it's get a good suport and a great confidence
+crimes_rulez <- apriori(crimes, parameter = list(minlen=4,maxlen=10, supp=0.55, conf=0.80))
 
 # Reduce the number of digits in measures of quality of the model
-quality(model) <- round(quality(model), digits=2)
+quality(crimes_rulez) <- round(quality(crimes_rulez), digits=2)
 
 # Sort by some measures
-model_srt_confidence <- sort(model, by="confidence")
+model_srt_confidence <- sort(crimes_rulez, by="confidence")
 
-model_srt_support <- sort(model, by="support")
+model_srt_support <- sort(crimes_rulez, by="support")
 
 
-#Check models 
+# Check models 
 inspect(model_srt_confidence)
 
 inspect(model_srt_support)
 
-# See specific rules of democrats
-democrats <- subset(model, subset = rhs %pin% "Class=democrat")
+# See specific rules of zones
+Zona_Sul <- subset(crimes_rulez, subset = rhs %pin% "Zona=Zona_Sul")
 
-democrats <- subset(model, subset = rhs %pin% "PericiaInvestigacao=nao")
+Zona_Norte <- subset(crimes_rulez, subset = rhs %pin% "Zona=Zona_Norte")
+
+Zona_Leste <- subset(crimes_rulez, subset = rhs %pin% "Zona=Zona_Leste")
+
+Zona_Oeste <- subset(crimes_rulez, subset = rhs %pin% "Zona=Zona_Oeste")
+
+Centro <- subset(crimes_rulez, subset = rhs %pin% "Zona=Centro")
+
+
+
+Iluminacao_Sim <- subset(crimes_rulez, subset = rhs %pin% "Iluminacao=Sim")
+
+
+Ocorrencia_Atendida_15_Minutos_Sim <- subset(crimes_rulez, subset = rhs %pin% "Ocorrencia_Atendida_15_Minutos=Sim")
 
 # See Rules
-inspect(democrats)
+inspect(Zona_Sul)
 
-# Top 20 rules of democrats
-inspect(democrats[1:20])
+# Top 20 rules of Zona_Sul
+inspect(Zona_Sul[1:20])
