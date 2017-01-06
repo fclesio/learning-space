5c445708b5e9a4bd9c007fc4f958ace9e267efc8
flavio-clesio-movile <flavio.clesio@movile.com>
2017-01-05 22:41:42 -0200

    ntrees = 10000 and grid consider max_depth = 30

--- h2o/GBM Deep Dive with Layman Brothers Bank.R
+++ h2o/GBM Deep Dive with Layman Brothers Bank.R
@@ -19,25 +19,30 @@ install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws
 library(h2o)
 
 # Start instance with all cores
-h2o.init(nthreads = -1, max_mem_size = "8G"))
+h2o.init(nthreads = -1, max_mem_size = "8G")
+
+h2o.clusterInfo()
 
 #Production Cluster (Not applicable)
 #localH2O <- h2o.init(ip = '10.112.81.210', port =54321, nthreads=-1) # Belo Vale 2
 #localH2O <- h2o.init(ip = '10.112.80.74', port =54321, nthreads=-1) # Belo Vale 1
 
 
-# GBM Demo
+# GBM
 
+# URL with data
 LaymanBrothersURL = "https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/02%20-%20Classification/default_credit_card.csv"
 
+# Load data 
 creditcard.hex = h2o.importFile(path = LaymanBrothersURL, destination_frame = "creditcard.hex")
 
 # Let's see the summary
 summary(creditcard.hex)
 
+# Convert DEFAULT variable to categorical
 creditcard.hex[,25] <- as.factor(creditcard.hex[,25])
 
-
+# We'll get 3 dataframes Train (60%), Test(20%) and Validation (20%)
 creditcard.split = h2o.splitFrame(data = creditcard.hex
                                   ,ratios = c(0.6,0.2)
                                   ,destination_frames = c("creditcard.train.hex", "creditcard.valid.hex", "creditcard.test.hex")
@@ -52,14 +57,19 @@ creditcard.test = creditcard.split[[2]]
 # Get the test dataframe(3rd split object)
 creditcard.validation = creditcard.split[[3]]
 
+
+# See datatables from each dataframe
 h2o.table(creditcard.train$DEFAULT)
 
 h2o.table(creditcard.test$DEFAULT)
 
 h2o.table(creditcard.validation$DEFAULT)
 
+
+# Set dependent variable
 Y = "DEFAULT"
 
+# Set independent variables
 X = c("LIMIT_BAL","EDUCATION","MARRIAGE","AGE"
       ,"PAY_0","PAY_2","PAY_3","PAY_4","PAY_5","PAY_6"
       ,"BILL_AMT1","BILL_AMT2","BILL_AMT3","BILL_AMT4","BILL_AMT5","BILL_AMT6"
@@ -67,6 +77,7 @@ X = c("LIMIT_BAL","EDUCATION","MARRIAGE","AGE"
 
 # Intentionally I removed sex variable from the model, to avoid put any gender bias inside the model. Ethics first guys! ;)
 
+# Train model
 creditcard.gbm <- h2o.gbm(y = Y, x = X, training_frame = creditcard.train
                           ,ntrees = 10,max_depth = 3,min_rows = 2
                           ,learn_rate = 0.2, distribution= "AUTO")
@@ -75,33 +86,48 @@ creditcard.gbm <- h2o.gbm(y = Y, x = X, training_frame = creditcard.train
 # To obtain the Mean-squared Error by tree from the model object:
 creditcard.gbm@model$scoring_history
 
+# See algo performance
 h2o.performance(creditcard.gbm, newdata = creditcard.test)
 
+# Variable importance
 h2o.varimp(creditcard.gbm)
 
 # Predict using GLM model
 pred = h2o.predict(object = creditcard.gbm, newdata = creditcard.test)
 
+# See predictions
 pred
 
 
 
 
 
-
+# Set hyparameters
 hyper_params = list( max_depth = seq(1,30,1) )
 
 
 grid <- h2o.grid(
   hyper_params = hyper_params, search_criteria = list(strategy = "Cartesian"), algorithm="gbm"
   ,grid_id="depth_grid", x = X, y = Y
-  ,training_frame = creditcard.train, validation_frame = creditcard.test, ntrees = 20
+  ,training_frame = creditcard.train, validation_frame = creditcard.validation, ntrees = 10000
   ,learn_rate = 0.05, learn_rate_annealing = 0.99, sample_rate = 0.8
   ,col_sample_rate = 0.8, seed = 1234, stopping_rounds = 5
   ,stopping_tolerance = 1e-4, stopping_metric = "AUC", score_tree_interval = 10
 )
 
 
+grid                                                                       
+
+## sort the grid models by decreasing AUC
+sortedGrid <- h2o.getGrid("depth_grid", sort_by="auc", decreasing = TRUE)    
+sortedGrid
+
+## find the range of max_depth for the top 5 models
+topDepths = sortedGrid@summary_table$max_depth[1:5]                       
+minDepth = min(as.numeric(topDepths))
+maxDepth = max(as.numeric(topDepths))
+minDepth
+maxDepth
 
 
 
