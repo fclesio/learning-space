f776006c030d4b294c62f8a1ed16c33c6932e6f4
flavio-clesio-movile <flavio.clesio@movile.com>
2017-01-05 22:06:49 -0200

    Layman brothers initial commit

---
+++ h2o/GBM Deep Dive with Layman Brothers Bank.R
@@ -0,0 +1,82 @@
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download packages that H2O depends on.
+if (! ("methods" %in% rownames(installed.packages()))) { install.packages("methods") }
+if (! ("statmod" %in% rownames(installed.packages()))) { install.packages("statmod") }
+if (! ("stats" %in% rownames(installed.packages()))) { install.packages("stats") }
+if (! ("graphics" %in% rownames(installed.packages()))) { install.packages("graphics") }
+if (! ("RCurl" %in% rownames(installed.packages()))) { install.packages("RCurl") }
+if (! ("jsonlite" %in% rownames(installed.packages()))) { install.packages("jsonlite") }
+if (! ("tools" %in% rownames(installed.packages()))) { install.packages("tools") }
+if (! ("utils" %in% rownames(installed.packages()))) { install.packages("utils") }
+
+# Now we download, install and initialize the H2O package for R.
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turing/8/R")))
+
+# Load library
+library(h2o)
+
+# Start instance with all cores
+h2o.init(nthreads = -1, max_mem_size = "8G"))
+
+#Production Cluster (Not applicable)
+#localH2O <- h2o.init(ip = '10.112.81.210', port =54321, nthreads=-1) # Belo Vale 2
+#localH2O <- h2o.init(ip = '10.112.80.74', port =54321, nthreads=-1) # Belo Vale 1
+
+
+# GBM Demo
+
+LaymanBrothersURL = "https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/02%20-%20Classification/default_credit_card.csv"
+
+creditcard.hex = h2o.importFile(path = LaymanBrothersURL, destination_frame = "creditcard.hex")
+
+# Let's see the summary
+summary(creditcard.hex)
+
+
+creditcard.split = h2o.splitFrame(data = creditcard.hex,ratios = 0.80, seed = 12345)
+
+# Get the train dataframe(1st split object)
+creditcard.train = creditcard.split[[1]]
+
+# Get the test dataframe(2nd split object)
+creditcard.test = creditcard.split[[2]]
+
+h2o.table(creditcard.train$DEFAULT)
+
+h2o.table(creditcard.test$DEFAULT)
+
+Y = "DEFAULT"
+
+X = c("LIMIT_BAL","EDUCATION","MARRIAGE","AGE"
+      ,"PAY_0","PAY_2","PAY_3","PAY_4","PAY_5","PAY_6"
+      ,"BILL_AMT1","BILL_AMT2","BILL_AMT3","BILL_AMT4","BILL_AMT5","BILL_AMT6"
+      ,"PAY_AMT1","PAY_AMT3","PAY_AMT4","PAY_AMT5","PAY_AMT6")
+
+# Intentionally I removed sex variable from the model, to avoid put any gender bias inside the model. Ethics first guys! ;)
+
+creditcard.gbm <- h2o.gbm(y = Y, x = X, training_frame = creditcard.train
+                          ,ntrees = 10,max_depth = 3,min_rows = 2
+                          ,learn_rate = 0.2, distribution= "gaussian")
+
+
+# To obtain the Mean-squared Error by tree from the model object:
+creditcard.gbm@model$scoring_history
+
+h2o.performance(creditcard.gbm, newdata = creditcard.test)
+
+h2o.varimp(creditcard.gbm)
+
+# Predict using GLM model
+pred = h2o.predict(object = creditcard.gbm, newdata = creditcard.test)
+
+pred
+
+
+
+
+
+
+
