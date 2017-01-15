7ff89d469c87bf2f749e969812249c074408672a
flavio-clesio-movile <flavio.clesio@movile.com>
2017-01-15 08:32:27 -0200

    Ajustments in grid in learning rate

--- h2o/GBM Deep Dive with Layman Brothers Bank.R
+++ h2o/GBM Deep Dive with Layman Brothers Bank.R
@@ -21,13 +21,14 @@ library(h2o)
 # Start instance with all cores
 h2o.init(nthreads = -1, max_mem_size = "8G")
 
+# Info about cluster
 h2o.clusterInfo()
 
 #Production Cluster (Not applicable)
-#localH2O <- h2o.init(ip = '10.112.81.210', port =54321, nthreads=-1) # Belo Vale 2
-#localH2O <- h2o.init(ip = '10.112.80.74', port =54321, nthreads=-1) # Belo Vale 1
+#localH2O <- h2o.init(ip = '10.112.81.210', port =54321, nthreads=-1) # Server 1
+#localH2O <- h2o.init(ip = '10.112.80.74', port =54321, nthreads=-1) # Server 2
 
-# GBM
+# Gradient Boosting Machines
 
 # URL with data
 LaymanBrothersURL = "https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/02%20-%20Classification/default_credit_card.csv"
@@ -35,13 +36,19 @@ LaymanBrothersURL = "https://raw.githubusercontent.com/fclesio/learning-space/ma
 # Load data 
 creditcard.hex = h2o.importFile(path = LaymanBrothersURL, destination_frame = "creditcard.hex")
 
+# Convert DEFAULT, SEX, EDUCATION, MARRIAGE variables to categorical
+creditcard.hex[,25] <- as.factor(creditcard.hex[,25]) # DEFAULT
+
+creditcard.hex[,3] <- as.factor(creditcard.hex[,3]) # SEX
+
+creditcard.hex[,4] <- as.factor(creditcard.hex[,4]) # EDUCATION
+
+creditcard.hex[,5] <- as.factor(creditcard.hex[,5]) # MARRIAGE
+
 # Let's see the summary
 summary(creditcard.hex)
 
-# Convert DEFAULT variable to categorical
-creditcard.hex[,25] <- as.factor(creditcard.hex[,25])
-
-# We'll get 3 dataframes Train (60%), Test(20%) and Validation (20%)
+# We'll get 3 dataframes Train (60%), Test (20%) and Validation (20%)
 creditcard.split = h2o.splitFrame(data = creditcard.hex
                                   ,ratios = c(0.6,0.2)
                                   ,destination_frames = c("creditcard.train.hex", "creditcard.test.hex", "creditcard.validation.hex")
@@ -60,10 +67,21 @@ creditcard.validation = creditcard.split[[3]]
 # See datatables from each dataframe
 h2o.table(creditcard.train$DEFAULT)
 
+# DEFAULT Count
+# 1       0 14047
+# 2       1  4030
+
 h2o.table(creditcard.test$DEFAULT)
+
+# DEFAULT Count
+# 1       0  4697
+# 2       1  1285
 
 h2o.table(creditcard.validation$DEFAULT)
 
+# DEFAULT Count
+# 1       0  4620
+# 2       1  1321
 
 # Set dependent variable
 Y = "DEFAULT"
@@ -74,16 +92,16 @@ X = c("LIMIT_BAL","EDUCATION","MARRIAGE","AGE"
       ,"BILL_AMT1","BILL_AMT2","BILL_AMT3","BILL_AMT4","BILL_AMT5","BILL_AMT6"
       ,"PAY_AMT1","PAY_AMT3","PAY_AMT4","PAY_AMT5","PAY_AMT6")
 
-# Intentionally I removed sex variable from the model, to avoid put any gender bias inside the model. Ethics first guys! ;)
+# I intentionally removed sex variable from the model, to avoid put any gender bias inside the model. Ethics first guys! ;)
 
 # Train model
 creditcard.gbm <- h2o.gbm(y = Y
                           ,x = X
                           ,training_frame = creditcard.train
-                          ,ntrees = 50
+                          ,ntrees = 100
                           ,validation_frame = creditcard.validation
                           ,seed = 12345
-                          ,max_depth = 10
+                          ,max_depth = 100
                           ,min_rows = 10
                           ,learn_rate = 0.2
                           ,distribution= "bernoulli"
@@ -95,16 +113,42 @@ creditcard.gbm <- h2o.gbm(y = Y
                           )
 
 
+# Check the main parameters of the model
 creditcard.gbm@parameters
 
 # To obtain the Mean-squared Error by tree from the model object:
 creditcard.gbm@model$scoring_history
 
 # See algo performance
-h2o.performance(creditcard.gbm, newdata = creditcard.test)
+h2o.performance(creditcard.gbm, newdata = creditcard.validation)
+
+# H2OBinomialMetrics: gbm
+
+# MSE:  0.1398498
+# RMSE:  0.373965
+# LogLoss:  0.4599217
+# Mean Per-Class Error:  0.2998072
+# AUC:  0.7589642
+# Gini:  0.5179284
+
+# Confusion Matrix for F1-optimal threshold:
+#   0    1    Error        Rate
+# 0      3891  806 0.171599   =806/4697
+# 1       550  735 0.428016   =550/1285
+# Totals 4441 1541 0.226680  =1356/5982
+
 
 # Variable importance
 h2o.varimp(creditcard.gbm)
+
+# Variable Importances: 
+#   variable relative_importance scaled_importance percentage
+# 1     PAY_0         3619.597656          1.000000   0.257411
+# 2 LIMIT_BAL         1262.526123          0.348803   0.089786
+# 3       AGE         1201.132080          0.331841   0.085420
+# 4 BILL_AMT1          781.192810          0.215823   0.055555
+# 5 EDUCATION          759.597046          0.209857   0.054019
+
 
 # Predict using GLM model
 pred = h2o.predict(object = creditcard.gbm, newdata = creditcard.test)
@@ -112,14 +156,40 @@ pred = h2o.predict(object = creditcard.gbm, newdata = creditcard.test)
 # See predictions
 pred
 
+# predict        p0         p1
+# 1       0 0.9765618 0.02343815
+# 2       0 0.7837817 0.21621831
+# 3       0 0.9832879 0.01671212
+# 4       0 0.8989057 0.10109427
+# 5       0 0.9795999 0.02040010
+# 6       1 0.7104697 0.28953029
 
 
 
 
 # Set hyparameters
-hyper_params = list( max_depth = seq(1,30,1) )
+#hyper_params = list( max_depth = seq(1,30,1) )
+
+max_depth_list <- list( max_depth = seq(1,30,1))
+ntrees_list <- list( ntrees = seq(50,300,50))
+#learnrate_list <- list( learnrate = seq(0.1,0.90,0.1))
+learnrate_list <- list(.10,.20,.30,.40,.50,.60,.70,.80,.90)
 
 
+hyper_parameters <- list(ntrees=ntrees_list)
+                         #,max_depth=maxdepth_list)
+                         #,learn_rate=learnrate_list)
+
+
+
+
+
+
+
+# See hyparameters
+hyper_params
+
+# Grid Search using GBM
 grid <- h2o.grid(
                   hyper_params = hyper_params
                   ,search_criteria = list(strategy = "Cartesian")
@@ -129,32 +199,47 @@ grid <- h2o.grid(
                   ,y = Y
                   ,training_frame = creditcard.train
                   ,validation_frame = creditcard.validation
-                  ,ntrees = 1000
-                  ,learn_rate = 0.05
                   ,learn_rate_annealing = 0.99
                   ,sample_rate = 0.8
                   ,col_sample_rate = 0.8
-                  ,seed = 1234
+                  ,seed = 12345
                   ,stopping_rounds = 5
                   ,stopping_tolerance = 1e-6
                   ,stopping_metric = "AUC"
                   ,score_tree_interval = 10
                 )
 
-
+# See grid parameters
 grid                                                                       
 
 ## sort the grid models by decreasing AUC
 sortedGrid <- h2o.getGrid("depth_grid", sort_by="auc", decreasing = TRUE)    
 sortedGrid
 
-## find the range of max_depth for the top 5 models
-topDepths = sortedGrid@summary_table$max_depth[1:5]                       
-minDepth = min(as.numeric(topDepths))
-maxDepth = max(as.numeric(topDepths))
-minDepth
-maxDepth
 
+# Grab the model_id based in AUC
+best_glm_model_id <- sortedGrid@model_ids[[1]]
 
+# The best model
+best_glm <- h2o.getModel(best_glm_model_id)
 
+# Summary
+summary(best_glm)
 
+# Get model and put inside a object
+model = best_glm
+
+# Prediction using the best model
+pred2 = h2o.predict(object = model, newdata = creditcard.validation)
+
+# Frame with predictions
+datasdet_pred = as.data.frame(pred2)
+
+# Write a csv file
+write.csv(datasdet_pred, file = "predictions.csv", row.names=TRUE)
+
+# Shutdown the cluster 
+h2o.shutdown()
+
+# Are you sure you want to shutdown the H2O instance running at http://localhost:54321/ (Y/N)? Y
+# [1] TRUE
