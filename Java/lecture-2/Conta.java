b54821237971c13f898dda69031ee6b7c47be74b
fclesio <flavioclesio@yahoo.com>
2018-01-09 10:25:21 -0200

    Programa.java funcionando

--- Java/lecture-2/Conta.java
+++ Java/lecture-2/Conta.java
@@ -11,23 +11,23 @@ class Conta {
 
 	//O que ela pode fazer
 	void saca (double quantidade){
-		System.out.println("Saldo anterior (ANTES SAQUE): " + THIS.saldo);
-		double novoSaldo = THIS.saldo - quantidade; //O THIS nos usamos quando quiser usar a referencia dentro do mesmo objeto. Ou seja, acessar os atributos da mesma instancia
-		THIS.saldo = novoSaldo;
-		System.out.println("Saldo posterior (DEPOIS SAQUE): " + THIS.saldo);
+		System.out.println("Saldo anterior (ANTES SAQUE): " + this.saldo);
+		double novoSaldo = this.saldo - quantidade; //O THIS nos usamos quando quiser usar a referencia dentro do mesmo objeto. Ou seja, acessar os atributos da mesma instancia
+		this.saldo = novoSaldo;
+		System.out.println("Saldo posterior (DEPOIS SAQUE): " + this.saldo);
 
 	}
 
 	void deposita(double quantidade){
-		System.out.println("Saldo anterior (ANTES DEPOSITO): " + THIS.saldo);
-		THIS.saldo = THIS.saldo + quantidade;
-		System.out.println("Saldo posterior (DEPOIS DEPOSITO): " + THIS.saldo);
+		System.out.println("Saldo anterior (ANTES DEPOSITO): " + this.saldo);
+		this.saldo = this.saldo + quantidade;
+		System.out.println("Saldo posterior (DEPOIS DEPOSITO): " + this.saldo);
 	}
 
 	void rendimento(double quantidade){
-		System.out.println("Saldo anterior (ANTES RENDIMENTO): " + THIS.saldo);
-		THIS.saldo = THIS.saldo * 0.1;
-		System.out.println("Saldo posterior (DEPOIS RENDIMENTO): " + THIS.saldo);
+		System.out.println("Saldo anterior (ANTES RENDIMENTO): " + this.saldo);
+		this.saldo = this.saldo * 0.1;
+		System.out.println("Saldo posterior (DEPOIS RENDIMENTO): " + this.saldo);
 	}
 
 }
