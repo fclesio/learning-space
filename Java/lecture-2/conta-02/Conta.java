e966fb3d5c21c9d590f7d2221b40f1ef02de0afc
fclesio <flavioclesio@yahoo.com>
2018-01-09 11:08:31 -0200

    Problema 2 em relação a atribuicao do metodo

--- Java/lecture-2/conta-02/Conta.java
+++ Java/lecture-2/conta-02/Conta.java
@@ -2,32 +2,36 @@ class Conta {
 	//Como nao vamos executar nada, e precisamos apenas da representacao
 	//do que e uma conta, entao nao teremos o main{}
 
-	//O que a conta tem
+	//Atributos, o que a conta tem
 	String titular;
 	int numero;
 	int agencia;
 	Double saldo;
 	String dataAniversario;
 
-	//O que ela pode fazer
-	void saca (double quantidade){
+	//Comportamentos, isto e o que a classe pode fazer. 
+	void saca(double valor){
 		System.out.println("Saldo anterior (ANTES SAQUE): " + this.saldo);
-		double novoSaldo = this.saldo - quantidade; //O THIS nos usamos quando quiser usar a referencia dentro do mesmo objeto. Ou seja, acessar os atributos da mesma instancia
-		this.saldo = novoSaldo;
+		if(this.saldo >= valor){ //O THIS nos usamos quando quiser usar a referencia dentro do mesmo objeto. Ou seja, acessar os atributos da mesma instancia
+			this.saldo = this.saldo - valor;
+		}
 		System.out.println("Saldo posterior (DEPOIS SAQUE): " + this.saldo);
-
 	}
 
-	void deposita(double quantidade){
+	void deposita(double valor){
 		System.out.println("Saldo anterior (ANTES DEPOSITO): " + this.saldo);
-		this.saldo = this.saldo + quantidade;
+		this.saldo = this.saldo + valor;
 		System.out.println("Saldo posterior (DEPOIS DEPOSITO): " + this.saldo);
 	}
 
-	void rendimento(double quantidade){
-		System.out.println("Saldo anterior (ANTES RENDIMENTO): " + this.saldo);
-		this.saldo = this.saldo * 0.1;
-		System.out.println("Saldo posterior (DEPOIS RENDIMENTO): " + this.saldo);
+	double calculaRendimento(){ //Fica em branco pois ele não recebe nenhum parâmetro	
+		return this.saldo * 0.1;
+	}
+
+	String recuperaDados(){
+		//Acessando o atributo e atribuit
+		String dados = "Nome: " + this.titular + "\nNumero: " + this.numero + "\nAgencia: " + this.agencia;
+		return dados;
 	}
 
 }
