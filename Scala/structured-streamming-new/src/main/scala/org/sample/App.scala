05b47666bb8baabd38d758f831a7c61419f9e666
fclesio <flavioclesio@gmail.com>
2018-11-30 19:17:33 +0100

    Commit for structured streaming

---
+++ Scala/structured-streamming-new/src/main/scala/org/sample/App.scala
@@ -0,0 +1,44 @@
+package org.sample
+
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Level, Logger}
+
+object App {
+
+  def main(args : Array[String]): Unit = {
+
+    // Turn off irrelevant logs
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val spark = SparkSession.builder()
+      .appName("Spark Structured Streaming Job")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+
+  }
+
+}
