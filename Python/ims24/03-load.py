6d2599c379e899626a478135d77f2254666124df
fclesio <flavioclesio@gmail.com>
2018-12-12 18:41:31 +0100

    Create a class for the load phase

--- Python/ims24/03-load.py
+++ Python/ims24/03-load.py
@@ -3,37 +3,55 @@
 
 import psycopg2
 import os
-from sqlalchemy import text
-from sqlalchemy import create_engine
 
-# Globals
-conn = psycopg2.connect(
-    host=os.environ['IMS_HOSTNAME'],
-    user=os.environ['IMS_USERNAME'],
-    password=os.environ['IMS_PASSWORD'],
-    dbname=os.environ['IMS_DB_NAME'],
-    port=os.environ['IMS_PORT'],
-    connect_timeout=5
-)
-conn.autocommit = True
+# Get Connections
+try:
+    print("Opening Connection")
+    conn = psycopg2.connect(host=os.environ['IMS_HOSTNAME'],
+                            user=os.environ['IMS_USERNAME'],
+                            password=os.environ['IMS_PASSWORD'],
+                            dbname=os.environ['IMS_DB_NAME'],
+                            port=os.environ['IMS_PORT'],
+                            connect_timeout=5)
+    conn.autocommit = True
+except Exception as e:
+    print(e)
+    print("ERROR: Could not connect into Analytics database. Check the connection.")
+    raise e
 
-def get_script_file_and_execute(filename):
-    file_read = open(filename, 'r')
-    sql_file = file_read.read()
-    file_read.close()
-    cursor = conn.cursor()
-    cursor.execute(sql_file)
-    cursor.close()
 
-etls = [ 'analytics-database/sql/03-process-staging.sql',
-        'analytics-database/sql/04-process-dimensions.sql',
-        'analytics-database/sql/05-process-fact.sql',
-        ]
+class Load(object):
+    def __init__(self):
+        super(Load, self).__init__()
 
-for etl_task in etls:
-    print ('########################################')
-    print ('Start of ETL stage: ', etl_task)
-    get_script_file_and_execute(etl_task)
-    print ('End of ETL stage: ', etl_task)
-    print ('########################################')
+    @staticmethod
+    def run_load():
+        etls = [ 'analytics-database/sql/03-process-staging.sql',
+                'analytics-database/sql/04-process-dimensions.sql',
+                'analytics-database/sql/05-process-fact.sql',
+                ]
 
+        for etl_task in etls:
+            print ('########################################')
+            print ('Start of ETL stage: ', etl_task)
+
+            print('Reading .sql file')
+            file_read = open(etl_task, 'r')
+            sql_file = file_read.read()
+            file_read.close()
+            cursor = conn.cursor()
+
+            print("Open cursor")
+            cursor.execute(sql_file)
+            cursor.close()
+
+            print ('End of ETL stage: ', etl_task)
+            print ('########################################')
+
+def main():
+    load = Load()
+    load.run_load()
+
+if __name__ == '__main__':
+    print 'Start working'
+    main()
